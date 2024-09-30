cd2951b714aba62e3f1886b84de1dced9b05bac9
JuanMunoz0810 <jpablomunoz@estudiante.uniajc.edu.co>
2024-09-30 21:06:35 +0000

    Terminado

--- Ejercicio56.java
+++ Ejercicio56.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
+
 public class Ejercicio56 {
     public static void main(String[] args) {
-        System.out.println("Mi primer programa de JAVA en Github");
+        double nota = 0.0;
+        int Aprobados = 0, noAprobados = 0;
+        Scanner leer = new Scanner(System.in);
+        System.out.print("Digite la cantidad de estudiantes: ");
+        int estudiantes = leer.nextInt();
 
+        for (int i= 1; i <= estudiantes; i++) {
+            System.out.print("Nota del estudiante" + i + ":");
+            nota = leer.nextDouble();
+
+            if (nota >= 5.0) {
+                Aprobados++;
+            } else {
+                noAprobados++;
+            }
+
+        }
+        System.out.println("Aprobados :" + Aprobados + "noAprobados" + noAprobados);
     }
 }
