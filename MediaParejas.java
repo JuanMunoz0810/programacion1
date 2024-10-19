fcfddcd374b9d363397ec736a74acb60ea347eac
JuanMunoz0810 <jpablomunoz@estudiante.uniajc.edu.co>
2024-10-19 22:36:25 +0000

    fin

--- MediaParejas.java
+++ MediaParejas.java
@@ -1,14 +1,24 @@
+import java.util.Scanner;
+
 public class MediaParejas {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         int N1, N2;
-        double M, MAX;
-       MAX = (N1 + N2)/2;
-       while (N2 <> 999) || (N1 <> 999){
-        M = (N1 + N2)/2;
-        if (M > MAX){
-            MAX = M;
+        double M, MAX = Double.NEGATIVE_INFINITY;
+
+        System.out.println("Ingrese dos numeros (999 para terminar): ");
+        N1 = scanner.nextInt();
+        N2 = scanner.nextInt();
+
+        while(N1 != 999 || N2 != 999){
+            M = (N1 + N2) / 2.0;
+            if(M > MAX){
+                MAX = M;
+            }
+            System.out.println("Ingrese dos numeros (999 para terminar): ");
+            N1 = scanner.nextInt();
+            N2 = scanner.nextInt();
         }
-       }
-      System.out.println("Media maxima= " + MAX);
+        System.out.println("Media maxima= " + MAX);
     }
 }
