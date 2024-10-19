db57c4362d295a8d39f2a7940ef52ae02d8a029a
JuanMunoz0810 <jpablomunoz@estudiante.uniajc.edu.co>
2024-10-19 22:18:23 +0000

    fin

--- ValormaximoyValorminimo.java
+++ ValormaximoyValorminimo.java
@@ -2,22 +2,23 @@ import java.util.Scanner;
 
 public class ValormaximoyValorminimo {
     public static void main(String[] args) {
-        Scanner Scanner = Scanner(System.in);
-        int I;
-        double MAX, MIN, NUMERO;
-        System.out.println("Ingrese numero");
-        NUMERO = Scanner.nextDouble();
-        MAX = NUMERO;
-        MIN = NUMERO;
-        for (I = 2; I <= 100; I++) {
-            if (NUMERO > MAX) {
-                MAX = NUMERO;
-            } else {
-                if (NUMERO < MIN) {
-                    MIN = NUMERO;
-                }
-            }
+    Scanner scanner = new Scanner(System.in);
+     int[] NUMEROS = new int[100];
+     for(int I = 0; I <= 100; I++){
+     System.out.println("Ingrese un numero: ");
+     NUMEROS[I] = scanner.nextInt();
+     }
+     int MAXIMO = Integer.MIN_VALUE;
+     int MINIMO = Integer.MAX_VALUE;
+     for(int I= 0; I <= 100; I++ ){
+        if(NUMEROS [I] > MAXIMO){
+            MAXIMO = NUMEROS[I];
         }
-        System.out.println("Maximo" + MAX + "Minimo" + MIN);
+        if(NUMEROS[I] < MINIMO){
+            MINIMO = NUMEROS[I];
+        }
+      }
+      System.out.println("MAXIMO" + MAXIMO);
+      System.out.println("MINIMO" + MINIMO);
     }
 }
